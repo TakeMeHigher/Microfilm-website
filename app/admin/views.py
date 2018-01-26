665fec0a12ead2530d03c0d2473d6186d318cae7
TakeMeHiger <492745473@qq.com>
2018-01-27 00:23:29 +0800

    后台登录注销页面搭建

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,16 @@
 #coding:utf8
+from flask import render_template,redirect,url_for
 from . import admin
 
 @admin.route("/")
 def index():
     return "<h3 style='color:blue'>this is admin</h3>"
+
+
+@admin.route("/login")
+def login():
+    return render_template('admin/login.html')
+
+@admin.route("/logout")
+def logout():
+    return redirect(url_for('admin.login'))
