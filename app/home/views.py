fdf9f35f106be2b14d1b273c2f8133cbb650d986
TakeMeHiger <492745473@qq.com>
2018-02-01 17:14:00 +0800

    会员日志显示

--- app/home/views.py
+++ app/home/views.py
@@ -1,21 +1,23 @@
-#coding:utf8
+# coding:utf8
 import os
 
 from . import home
-from flask import render_template,redirect,url_for,request,session
+from flask import render_template, redirect, url_for, request, session
 from werkzeug.utils import secure_filename
 
-from .forms import RegForm,LoginForm,UserForm,PwdForm
+from .forms import RegForm, LoginForm, UserForm, PwdForm
 from app import models
 from  app import db
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__)).strip('\home')
 print(BASEDIR)
 
-file_dir=os.path.join(BASEDIR,'static','avatar')
+file_dir = os.path.join(BASEDIR, 'static', 'avatar')
 
 if not os.path.exists(file_dir):
     os.makedirs(file_dir)
+
+
 def getUser():
     '''
     获取当前登录的会员
@@ -25,6 +27,7 @@ def getUser():
     user = db.session.query(models.User).filter_by(name=username).first()
     return user
 
+
 @home.route("/")
 def index():
     '''
@@ -33,30 +36,32 @@ def index():
     '''
     return render_template('/home/index.html')
 
-@home.route('/login/',methods=['GET','POST'])
+
+@home.route('/login/', methods=['GET', 'POST'])
 def login():
     '''
     会员登录
     :return:
     '''
-    if request.method=='POST':
-        form =LoginForm(request.form)
+    if request.method == 'POST':
+        form = LoginForm(request.form)
         if form.validate():
-            data=form.data
-            user=db.session.query(models.User).filter_by(name=data.get('name'),pwd=data.get('pwd')).first()
+            data = form.data
+            user = db.session.query(models.User).filter_by(name=data.get('name'), pwd=data.get('pwd')).first()
             if user:
-                session['user']=data.get('name')
-                user=getUser()
-                userlog=models.Userlog(ip=request.remote_addr,user_id=user.id)
+                session['user'] = data.get('name')
+                user = getUser()
+                userlog = models.Userlog(ip=request.remote_addr, user_id=user.id)
                 db.session.add(userlog)
                 db.session.commit()
                 return redirect(url_for('home.index'))
             else:
-                msg='密码错误'
-                return render_template('home/login.html', form=form,msg=msg)
+                msg = '密码错误'
+                return render_template('home/login.html', form=form, msg=msg)
         return render_template('home/login.html', form=form)
-    form=LoginForm()
-    return  render_template('home/login.html',form=form)
+    form = LoginForm()
+    return render_template('home/login.html', form=form)
+
 
 @home.route('/logout/')
 def logout():
@@ -64,17 +69,21 @@ def logout():
     会员注销
     :return:
     '''
-    session['user']=''
-    return  redirect(url_for('home.login'))
+    session['user'] = ''
+    return redirect(url_for('home.login'))
 
 
-@home.route('/regist/',methods=['GET','POST'])
+@home.route('/regist/', methods=['GET', 'POST'])
 def regist():
-    if request.method=='POST':
-        form =RegForm(request.form)
+    '''
+    会员注册
+    :return:
+    '''
+    if request.method == 'POST':
+        form = RegForm(request.form)
         if form.validate():
-            data=form.data
-            user=models.User(
+            data = form.data
+            user = models.User(
                 name=data.get('name'),
                 pwd=data.get('pwd'),
                 email=data.get('email'),
@@ -84,35 +93,41 @@ def regist():
             db.session.commit()
             return redirect(url_for('home.login'))
         return render_template('/home/register.html', form=form)
-    form=RegForm()
-    return  render_template('/home/register.html',form=form)
+    form = RegForm()
+    return render_template('/home/register.html', form=form)
 
 
-@home.route('/user/',methods=['GET','POST'])
+@home.route('/user/', methods=['GET', 'POST'])
 def user():
-    if request.method=='POST':
-        form=UserForm(request.form)
-        file=request.files['avatar']
-        filename=secure_filename(file.filename)
-        file.save(os.path.join(file_dir,filename))
-        data=form.data
-        avatar=data.get('avatar')
-        user=getUser()
+    '''
+    用户信息展示
+    :return:
+    '''
+    if request.method == 'POST':
+        form = UserForm(request.form)
+        file = request.files['avatar']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(file_dir, filename))
+        data = form.data
+        avatar = data.get('avatar')
+        user = getUser()
         db.session.query(models.User).filter_by(id=user.id).update({
-            'name':data.get('name'),
-            'pwd':data.get('pwd'),
-            'email':data.get('email'),
-            'phone':data.get('phone'),
-            'avatar':data.get(avatar),
-            'info':data.get('info')
+            'name': data.get('name'),
+            'pwd': data.get('pwd'),
+            'email': data.get('email'),
+            'phone': data.get('phone'),
+            'avatar': data.get(avatar),
+            'info': data.get('info')
         })
         db.session.commit()
         return redirect(url_for('home.user'))
-    user=getUser()
-    form=UserForm(data={'name':user.name,'email':user.email,'phone':user.phone,'avatar':user.avatar,'info':user.info})
-    return  render_template('/home/user.html',form=form)
+    user = getUser()
+    form = UserForm(
+        data={'name': user.name, 'email': user.email, 'phone': user.phone, 'avatar': user.avatar, 'info': user.info})
+    return render_template('/home/user.html', form=form)
 
-@home.route('/changpwd/',methods=['GET','POST'])
+
+@home.route('/changpwd/', methods=['GET', 'POST'])
 def changpwd():
     '''
        修改密码
@@ -134,18 +149,20 @@ def changpwd():
                 return render_template('/home/changpwd.html', form=form, msg=msg)
         return render_template('/home/changpwd.html', form=form)
     form = PwdForm()
-    return  render_template('/home/changpwd.html',form=form)
+    return render_template('/home/changpwd.html', form=form)
+
 
 @home.route('/comments/')
 def comments():
-    return  render_template('/home/comments.html')
+    return render_template('/home/comments.html')
+
 
 @home.route('/loginlog/')
 def loginlog():
-    return  render_template('/home/loginlog.html')
+    userlogs=db.session.query(models.Userlog).all()
+    return render_template('/home/loginlog.html',userlogs=userlogs)
 
 
 @home.route('/moviecol/')
 def moviecol():
-    return  render_template('/home/moviecol.html')
-
+    return render_template('/home/moviecol.html')
